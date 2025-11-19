d738b9fd3be0b878d33117cfe56b427c402ea52d
phamnhi1604 <105583942+phamnhi1604@users.noreply.github.com>
2025-11-19 22:21:59 +0700

    giaodienLogin

--- AcentyShop/AcentyShop_Applicate/GUI/Feature/frmDonHang.Designer.cs
+++ AcentyShop/AcentyShop_Applicate/GUI/Feature/frmDonHang.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.uiDataGridView1 = new Sunny.UI.UIDataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.uiDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uiDataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.uiDataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.uiDataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.uiDataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.uiDataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.uiDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.uiDataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.uiDataGridView1.EnableHeadersVisualStyles = false;
+            this.uiDataGridView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uiDataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            this.uiDataGridView1.Location = new System.Drawing.Point(329, 155);
+            this.uiDataGridView1.Name = "uiDataGridView1";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.uiDataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uiDataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.uiDataGridView1.SelectedIndex = -1;
+            this.uiDataGridView1.Size = new System.Drawing.Size(459, 283);
+            this.uiDataGridView1.StripeOddColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.uiDataGridView1.TabIndex = 0;
+            // 
+            // frmDonHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uiDataGridView1);
+            this.Name = "frmDonHang";
             this.Text = "frmDonHang";
+            ((System.ComponentModel.ISupportInitialize)(this.uiDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Sunny.UI.UIDataGridView uiDataGridView1;
     }
 }
--- AcentyShop/AcentyShop_Applicate/GUI/frmDangNhap.Designer.cs
+++ AcentyShop/AcentyShop_Applicate/GUI/frmDangNhap.Designer.cs
@@ -30,48 +30,83 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.uiAvatar1 = new Sunny.UI.UIAvatar();
-            this.uiMenuButton1 = new Sunny.UI.UIMenuButton();
+            this.uiButton1 = new Sunny.UI.UIButton();
+            this.txtUsername = new Sunny.UI.UITextBox();
+            this.txtPass = new Sunny.UI.UITextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(291, 51);
+            this.label1.Location = new System.Drawing.Point(294, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(219, 45);
+            this.label1.Size = new System.Drawing.Size(190, 45);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Login System";
+            this.label1.Text = "Đăng Nhập";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // uiAvatar1
             // 
             this.uiAvatar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.uiAvatar1.Location = new System.Drawing.Point(80, 151);
+            this.uiAvatar1.Location = new System.Drawing.Point(79, 117);
             this.uiAvatar1.MinimumSize = new System.Drawing.Size(1, 1);
             this.uiAvatar1.Name = "uiAvatar1";
             this.uiAvatar1.Size = new System.Drawing.Size(140, 139);
             this.uiAvatar1.TabIndex = 1;
             this.uiAvatar1.Text = "uiAvatar1";
             // 
-            // uiMenuButton1
+            // uiButton1
             // 
-            this.uiMenuButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.uiMenuButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.uiMenuButton1.Location = new System.Drawing.Point(299, 198);
-            this.uiMenuButton1.Menu = null;
-            this.uiMenuButton1.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiMenuButton1.Name = "uiMenuButton1";
-            this.uiMenuButton1.Size = new System.Drawing.Size(221, 35);
-            this.uiMenuButton1.TabIndex = 2;
-            this.uiMenuButton1.Text = "uiMenuButton1";
-            this.uiMenuButton1.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.uiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uiButton1.Location = new System.Drawing.Point(252, 214);
+            this.uiButton1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiButton1.Name = "uiButton1";
+            this.uiButton1.Size = new System.Drawing.Size(270, 31);
+            this.uiButton1.TabIndex = 2;
+            this.uiButton1.Text = "Đăng nhập";
+            this.uiButton1.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.uiButton1.Click += new System.EventHandler(this.uiButton1_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtUsername.Location = new System.Drawing.Point(252, 138);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUsername.MinimumSize = new System.Drawing.Size(1, 16);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Padding = new System.Windows.Forms.Padding(5);
+            this.txtUsername.ShowText = false;
+            this.txtUsername.Size = new System.Drawing.Size(270, 29);
+            this.txtUsername.TabIndex = 5;
+            this.txtUsername.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txtUsername.Watermark = "";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtPass.Location = new System.Drawing.Point(252, 177);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPass.MinimumSize = new System.Drawing.Size(1, 16);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Padding = new System.Windows.Forms.Padding(5);
+            this.txtPass.ShowText = false;
+            this.txtPass.Size = new System.Drawing.Size(270, 29);
+            this.txtPass.TabIndex = 6;
+            this.txtPass.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txtPass.Watermark = "";
             // 
             // frmDangNhap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.uiMenuButton1);
+            this.ClientSize = new System.Drawing.Size(659, 354);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.uiButton1);
             this.Controls.Add(this.uiAvatar1);
             this.Controls.Add(this.label1);
             this.Name = "frmDangNhap";
@@ -85,6 +120,8 @@
 
         private System.Windows.Forms.Label label1;
         private Sunny.UI.UIAvatar uiAvatar1;
-        private Sunny.UI.UIMenuButton uiMenuButton1;
+        private Sunny.UI.UIButton uiButton1;
+        private Sunny.UI.UITextBox txtUsername;
+        private Sunny.UI.UITextBox txtPass;
     }
 }
